74fa2c4cf7b14bf427098276ffd0677f349ecf1f
LekoevNVK <lekoev.nvk@gmail.com>
2022-08-01 13:06:33 +0500

    MVP for tests

---
+++ tests/conftest.py
@@ -0,0 +1,31 @@
+# Fixtures - decorators for functions
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as chrome_options
+
+
+@pytest.fixture
+def get_chrome_options():
+    options = chrome_options()
+    options.add_argument('chrome')  # Use headless if you do not need browser UI
+    options.add_argument('--start-maximized')  # Start browser on fullscreen mode
+    options.add_argument('--windows-size=1280,720')
+    return options
+
+
+@pytest.fixture
+def get_webdriver(get_chrome_options):
+    options = get_chrome_options
+    driver = webdriver.Chrome(options=options)  # If driver not in python folder use executable_path='C:/Users/...'
+    return driver
+
+
+@pytest.fixture
+def setup(request, get_webdriver):
+    driver = get_webdriver
+    url = 'https://www.dns-shop.ru'
+    if request.cls is not None:
+        request.cls.driver = driver
+    driver.get(url)
+    yield driver
+    driver.quit()
